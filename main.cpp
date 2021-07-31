f911ec8a4a508aa449c8d8217fb7f00ba2fb3f17
Vinay Maheshwari <67011829+Coding-Thunder@users.noreply.github.com>
2021-07-31 09:21:04 +0000

    Created CLI

--- main.cpp
+++ main.cpp
@@ -1,83 +1,78 @@
-// Merge Sort
-
-// #include <iostream>
-
-// using namespace std;
-
-// void merge(int arr[], int l, int mid, int r){
-//   int i = l;
-//   int j = mid+1;
-//   int k = l;
-//   int temp[r+1];
-//   while(i<=mid && j<=r) {
-//     if(arr[i] < arr[j]){
-//       temp[k] = arr[i];
-//       i++;
-//     } else{
-//       temp[k] = arr[j];
-//       j++;
-//     }
-//     k++;
-//   } 
-//   while(i<=mid) {
-//     temp[k] = arr[i];
-//     i++;
-//     k++;
-//   }
-//   while(j<=r) {
-//     temp[k] = arr[j];
-//     j++;
-//     k++;
-//   }
-
-//   for(int s=l;s<=r;s++){
-//     arr[s] = temp[s];
-//   }
-// }
-
-
-// void mergeSort(int arr[], int l, int r) {
-//   if(l<r){
-//     int mid = (l+r)/2;
-//     mergeSort(arr, l, mid);
-//     mergeSort(arr, mid+1, r);
-//     merge(arr,l,mid,r);
-//   }
-// }
-
-// int main() {
-//   int n;
-//   cout << " Welcome to my merge sort programme" << endl << endl;
-//   cout << "Enter length of array : ";
-//   cin >> n;
-//   cout << endl;
-//   int arr[n];
-//   for(int i=0; i<n; i++) {
-//     cout << "Enter element at index " << i << " : ";
-//     cin  >> arr[i] ;
-//     cout << endl;
-//   }
-
-//   cout << "* Before Merge Sort :-" << endl<< endl;
-//   for(int x : arr) {
-//     cout << x << " ";
-//   }
-//   mergeSort(arr,0,n-1);
-//   cout << endl << endl;
-//   cout << "* After Merge Sort :-" << endl<< endl;
-//   for(int x : arr) {
-//     cout << x << " ";
-//   }
-// }
-
-// Selection Sort //
-
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int main() {
+void merge(int arr[], int l, int mid, int r){
+  int i = l;
+  int j = mid+1;
+  int k = l;
+  int temp[r+1];
+  while(i<=mid && j<=r) {
+    if(arr[i] < arr[j]){
+      temp[k] = arr[i];
+      i++;
+    } else{
+      temp[k] = arr[j];
+      j++;
+    }
+    k++;
+  } 
+  while(i<=mid) {
+    temp[k] = arr[i];
+    i++;
+    k++;
+  }
+  while(j<=r) {
+    temp[k] = arr[j];
+    j++;
+    k++;
+  }
+
+  for(int s=l;s<=r;s++){
+    arr[s] = temp[s];
+  }
+}
+
+
+void mergeSort(int arr[], int l, int r) {
+  if(l<r){
+    int mid = (l+r)/2;
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid+1, r);
+    merge(arr,l,mid,r);
+  }
+}
+
+void performMergeSort() {
+  int n;
+  cout << " Welcome to my merge sort programm" << endl << endl;
+  cout << "Enter length of array : ";
+  cin >> n;
+  cout << endl;
+  int arr[n];
+  for(int i=0; i<n; i++) {
+    cout << "Enter element at index " << i << " : ";
+    cin  >> arr[i] ;
+    cout << endl;
+  }
+
+  cout << "* Before Merge Sort :-" << endl<< endl;
+  for(int x : arr) {
+    cout << x << " ";
+  }
+  mergeSort(arr,0,n-1);
+  cout << endl << endl;
+  cout << "* After Merge Sort :-" << endl<< endl;
+  for(int x : arr) {
+    cout << x << " ";
+  }
+  cout <<endl;
+}
+
+
+
+int performSelectionSort() {
   vector<int> A = {10,9,10000,7,6,5,4,3,2,1,0};
   int i = 0,j,min_index;
   while(i<A.size()-1) {
@@ -94,6 +89,34 @@ int main() {
   }
   for(int x : A){
     cout << x << endl;
+  }
+  return 0;
+}
+
+int main() {
+  int res;
+  cout << "What do you want to do ?" <<endl;
+  cout << " 1 for Merge Sort "<<endl;
+  cout << " 2 for Selection Sort "<<endl;
+  cout << " 3 to exit  "<<endl;
+  cin  >>res;
+
+  switch (res) {
+    case 1: {
+      performMergeSort();
+      break;
+    }
+    case 2:{
+      performSelectionSort();
+      break;
+    }
+    case 3:{
+      break;
+    }
+    default:{
+    cout << "Wrong input"<<endl << endl;
+    main();
+    }
   }
   return 0;
 }
